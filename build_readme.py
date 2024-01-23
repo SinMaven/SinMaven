3b3240060567375233699602068b7c3723f715a0
Rinx <96206184+0xRinx@users.noreply.github.com>
2024-01-23 04:20:02 +0200

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -62,10 +62,11 @@ if __name__ == "__main__":
    entries = fetch_blog_entries()[:3]
    entries_md = "\n".join(
        # ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
-       ["| | * [{title}]({url}) |".format(**entry) for entry in entries]
+       ["| | ~ [{title}]({url}) |".format(**entry) for entry in entries]
    )
    rewritten = replace_chunk(readme_contents, "blog", entries_md)
 
+   entries_md += "\n"
    til_readme_contents = get_tils()
 
    print('~' * 50)

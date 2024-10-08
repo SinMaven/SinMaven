3a62794eba662bac3449760c3a5343155c6e730f
SinMaven <96206184+SinMaven@users.noreply.github.com>
2024-10-09 00:04:06 +0300

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -1,89 +1,82 @@
 import feedparser
+import httpx
 import pathlib
 import re
+import os
 import requests
+import git
 
-# Set the root directory for the script
 root = pathlib.Path(__file__).parent.resolve()
 
 def replace_chunk(content, marker, chunk, inline=True):
-    """Replace a specified chunk in the content between markers with a new chunk."""
-    r = re.compile(
-        r"<!-- {} starts -->.*<!-- {} ends -->".format(marker, marker),
-        re.DOTALL,
-    )
-    if not inline:
-        chunk = "\n{}".format(chunk)
-    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
-    return r.sub(chunk, content)
+   r = re.compile(
+       r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+       re.DOTALL,
+   )
+   if not inline:
+       chunk = "\n{}\n".format(chunk)
+   chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+   return r.sub(chunk, content)
 
 def get_tils():
-    """Fetch the latest TIL entries from the specified README."""
-    til_readme_url = "https://raw.githubusercontent.com/SinMaven/TIL/master/README.md"
-    response = requests.get(til_readme_url)
-    response.raise_for_status()  # Ensure the request was successful
+   til_readme = "https://raw.githubusercontent.com/SinMaven/TIL/master/README.md"
+   r = requests.get(til_readme)
 
-    all_text = response.text
-    search_re = re.findall(r'(\*+).(\[.*?\])(\(.*?\)).?-(.+)', all_text, re.M | re.I)
-    dt_til = sorted(search_re, key=lambda search_re: search_re[3], reverse=True)[:3]
+   page = requests.get(til_readme)
+   all_text = page.text
+   search_re = re.findall( r'(\*+).(\[.*?\])(\(.*?\)).?-(.+)', all_text, re.M|re.I)
+   dt_til = sorted(search_re, key=lambda search_re: search_re[3], reverse=True)[:3]
 
-    print('~' * 50)
-    print('Latest TIL entries (up to 3):', dt_til)
-    print('~' * 50)
+   print('~' * 50)
+   print('dt_til upto 3', dt_til)
+   print('~' * 50)
 
-    til_md = "\n".join(f"{i[0]} {i[1]}{i[2]}" for i in dt_til)
-    
-    print('~' * 50)
-    print('Formatted TIL entries:', til_md)
-    print('~' * 50)
+   til_md = ""
 
-    return til_md
+   for i in dt_til:
+       til_md += "\n" + i[0] + ' ' + i[1] + i[2]        
+
+   print('~' * 50)
+   print('til_md upto 3', til_md)
+   print('~' * 50)
+
+   return til_md
 
 def fetch_blog_entries():
-    """Fetch the latest blog entries from the RSS feed."""
-    entries = feedparser.parse("https://SinMaven.github.io/rss.xml")["entries"]
-    return [
-        {
-            "title": entry["title"],
-            "url": entry["link"].split("#")[0],
-            "published": entry["published"].split("T")[0],
-        }
-        for entry in entries
-    ]
+   entries = feedparser.parse("https://SinMaven.github.io/rss.xml")["entries"]
+   return [
+       {
+           "title": entry["title"],
+           "url": entry["link"].split("#")[0],
+           "published": entry["published"].split("T")[0],
+       }
+       for entry in entries
+   ]
+
 
 if __name__ == "__main__":
-    readme_path = root / "README.md"
-    
-    # Read the current content of the README
-    with open(readme_path, "r") as file:
-        readme_contents = file.read()
 
-    # Fetch and format the latest blog entries
-    entries = fetch_blog_entries()[:3]
-    entries_md = "<br>".join(
-        [f"• [{entry['title']}]({entry['url']})" for entry in entries]
-    )
+   readme = root / "README.md"
+   readme_contents = readme.open().read()
 
-    # Ensure a single new line before inserting blog posts
-    entries_md = "\n" + entries_md  # Add a new line before blog posts
+   entries = fetch_blog_entries()[:3]
+   entries_md = "<br>".join(
+       [f"• [{title}]({url})".format(**entry) for entry in entries]
+   )
+   entries_md = "\n" + entries_md
+   rewritten = replace_chunk(readme_contents, "blog", entries_md)
+   
+   til_readme_contents = get_tils()
 
-    # Replace the blog section in the README
-    rewritten = replace_chunk(readme_contents, "blog", entries_md)
+   print('~' * 50)
+   print('til_readme_contents', til_readme_contents)
+   print('~' * 50)
 
-    # Fetch and format the latest TIL entries
-    til_readme_contents = get_tils()
+   rewritten = replace_chunk(rewritten, "tilentries", til_readme_contents)   
 
-    print('~' * 50)
-    print('TIL README contents:', til_readme_contents)
-    print('~' * 50)
+   print('~' * 50)
+   print('rewritten after getting tils', rewritten)
+   print('~' * 50)
 
-    # Replace the TIL entries in the README
-    rewritten = replace_chunk(rewritten, "tilentries", til_readme_contents)
 
-    print('~' * 50)
-    print('Rewritten content after updating TILs:', rewritten)
-    print('~' * 50)
-
-    # Write the updated content back to the README
-    with open(readme_path, "w") as file:
-        file.write(rewritten)
+   readme.open("w").write(rewritten)
